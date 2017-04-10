986f09c3bb83af2893b07a8b79d386b1e2570468
Liam Ellis <liamhellis@gmail.com>
2017-04-10 17:31:32 -0400

    tweaks

--- config.js
+++ config.js
@@ -4,9 +4,7 @@ export default {
     title: 'Theresa on the Town'
   },
   bucket: {
-    slug: process.env.COSMIC_BUCKET || 'deploy-demo',
-    media_url: 'https://cosmicjs.com/uploads',
-    read_key: process.env.COSMIC_READ_KEY || '',
-    write_key: process.env.COSMIC_WRITE_KEY || ''
+    slug: 'deploy-demo',
+    media_url: 'https://cosmicjs.com/uploads'
   },
 }
